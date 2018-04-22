61129fb964d47c34f6e227a97c9ee11017761984
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-04-22 14:31:30 -0500

    createUser

--- database/queries.js
+++ database/queries.js
@@ -5,17 +5,22 @@ const db = require('./client')
 const findAll = () => {
   return db.many(`SELECT * FROM users`).then(
     results => results,
-    error => error
+    error   => error
   )
 }
 
-const findOne = () => {
-  // TODO: Find a single user based on name
-  // return db.one(`SELECT`)
+const findOne = (name) => {
+  return db.one(`SELECT * FROM users WHERE name = $1`, [name]).then(
+    results => results,
+    error   => error
+  )
 }
 
-const createUser = () => {
-  // TODO: Insert a new user into the table
+const createUser = (user) => {
+  return db.none(`INSERT INTO users (name, password) VALUES ($1, $2)`, [user.name, user.password]).then(
+    results => results,
+    error   => error
+  )
 }
 
 module.exports = {
--- routes/signup.js
+++ routes/signup.js
@@ -1,33 +1,36 @@
 // jshint asi:true
 const router = require('express').Router()
-const { findAll } = require('../database/queries')
+const { findAll,
+        findOne,
+        createUser } = require('../database/queries')
 
 router.get('/', (req, res) => {
   res.render('signup')
 })
 
 router.post('/', (req, res) => {
-  // TODO: Determine if all fields are present
-  if (req.body.name === '' || req.body.password === '' || req.body.passwordConfirm === '' ) {
+  const user = req.body
+
+  // Determine if all fields are present
+  if (user.name === '' || user.password === '' || user.passwordConfirm === '' ) {
     return res.render('signup', { message: 'Please provide a username and password' })
   }
 
-  // TODO: Determine if passwords match
-  if (req.body.password != req.body.passwordConfirm) {
+  // Determine if password confirmation matches
+  if (user.password != user.passwordConfirm) {
     return res.render('signup', { message: 'Your password did not match' })
   }
 
   // TODO: Determine if name is taken - stretch
 
-  // TODO: Store data to database, render index
-
-  findAll().then(
+  // Store data to database, render index
+  createUser(user).then(
     results => {
-      return res.render('signup', {message: `${results[0].name} and ${results[1].name}`}),
+      return res.render('index', {name: user.name, message: `User Created`}),
     error => {
       return res.render('signup', {message: error})
-      }
-    })
+    }
+  })
 })
 
 module.exports = router
